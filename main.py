7355a7219eb2155b1c55b7bc6c05c9079dc4e4b1
JaeGeun.Song <jae99c@gmail.com>
2021-08-19 14:49:21 +0900

    path parameter practice

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-# API에 대한 모든 기능을 제공하는 파이썬 클래스 FastAPI
-# Starlette를 직접 상속하는 클래스로 Starlette의 모든 기능을 사용가능하다.
 
-app = FastAPI() #FastAPI 클래스의 인스턴스
-# uvicorn main:fastapi --reload
+app = FastAPI()
 
 """
     POST : 데이터 생성
@@ -12,7 +9,7 @@ app = FastAPI() #FastAPI 클래스의 인스턴스
     DELETE : 데이터 삭제
 """
 
-@app.get("/") # 경로는 첫번째 / 에서 시작하는 URL의 마지막 부분이다.
-# get동작을 사용하여 URL "/"에 대한 요청을 받을 때마다 FastAPI에 의해 호출된다.
-async def root(): # / 경로가 실행될때 실행하는 함수 , async는 비동기 형식
-    return {"message":"Hello World"} # dict, list 단일값을 가진 str int 등을 반환 할 수 있다.
+# 파이썬 포맷 문자열이 사용하는 동일한 문법으로 "매개 변수" 또는 "변수"를 경로에 선언할 수 있다.
+@app.get("/items/{item_id}")
+async def read_item(item_id):  # 경로 매개변수 item_id의 값은 함수의 item_id 인자로 전달된다.
+    return {"item_id":item_id}
