53d6d3c0120f47be56f25681320692578e8a0538
JaeGeun.Song <jae99c@gmail.com>
2021-08-19 15:03:58 +0900

    order problem about pathwork

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app =FastAPI()
+
+@app.get("/users/me") # 이건 고정경로
+async def read_user_me():
+    return {"user_id" : "the current user"}
+
+@app.get("/users/{user_id}") # 이건 매개변수 경로
+async def read_user(user_id: str):
+    return {"user_id" : user_id}
 
 """
-    POST : 데이터 생성
-    GET : 데이터 읽기
-    PUT : 데이터 업데이트
-    DELETE : 데이터 삭제
-"""
-
-# 파이썬 포맷 문자열이 사용하는 동일한 문법으로 "매개 변수" 또는 "변수"를 경로에 선언할 수 있다.
-@app.get("/items/{item_id}")
-async def read_item(item_id : int):  # 경로 매개변수 item_id의 값은 함수의 item_id 인자로 전달된다.
-    return {"item_id":item_id} # 파이썬 표준 타입 어노테이션을 이용해 함수의 경로 매개변수의 타입을 선언할 수 있다.
-# 이 상태에서 매개변수가 int형이 아니라면 오류 발생
-
-""" 
-    다양한 도구가 존재한다.
-    Pydantic - 모든 데이터 검증은 내부적으로 수행되고 이로 인해 이득을 얻는다.
+    고정 경로와 매개변수 경로가 같이 있으면 고정 경로를 먼저 선언하고 후에 매개변수 경로를 선언해야한다.
+    /users/{user_id} 이전에 /users/me를 선언해야한다.
+    
+    그렇지 않으면 /users/{user_id} 는 매개변수 user_id의 값을 me라고 생각하여 /users/me도 연결한다.
 """
