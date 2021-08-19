1ebb7cfca31daa37554dd4c8b63b638af034d06c
JaeGeun.Song <jae99c@gmail.com>
2021-08-19 16:40:51 +0900

    multiple path and query parameter

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 """
-선택적 매개변수 Optional Parameter
-같은 방법으로 기본값을 None으로 설정하여 선택적 매개변수를 선언할 수 있다.
+여러 경로/쿼리 매개변수
+multiple path and query parameter
+
+경로 매개변수와 쿼리 매개변수를 동시에 선언할 수 있고 fastAPI는 어느것이 무엇인지 알고 있다.
+그리고 특정 순서로 선언할 필요없다.
+
+매개변수들은 이름으로 감지된다.
 """
 from typing import Optional
 from fastapi import FastAPI
 
-app = FastAPI()
+app =FastAPI()
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Optional[str] = None, short: bool = False): # bool형으로 선언할 수도 있다.
-    item = {"item_id": item_id}
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id:str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id":item_id, "owner_id":user_id}
     if q:
-        item.update({"q": q})
-    if not short: # short 값이 있는가 없는가를 bool로 판단한다.
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+        item.update({"q":q})
+    if not short:
+        item.update({"description":"This is an amazing item that has a long description"})
     return item
