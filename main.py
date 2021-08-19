e2b94cb9ee9e7cbe1c0f1ac1527d656f37f69f50
JaeGeun.Song <jae99c@gmail.com>
2021-08-19 16:54:08 +0900

    essential query parameter level 2

--- main.py
+++ main.py
@@ -6,13 +6,26 @@ essential query parameter level 1
 
 쿼리 매개변수를 필수로 만들려면 기본값을 선언할 수 없다.
 """
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_user_item(item_id: str, needy: str):
-    item = {"item_id": item_id, "needy": needy}  # 쿼리 매개변수의 기본값을 선언하지 않으면 필수가 된다.
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: Optional[int] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
-#needy는 필수 매개변수 이므로 반드시 URL에 설정해주어야한다.
+"""
+경로 매개변수 - item_id
+
+쿼리 매개변수 
+- needy (필수)
+- skip (기본값=0)
+- limit (선택 매개변수 : 기본값 None)
+"""
+
+# 팁 : 경로 매개변수와 마찬가지로 Enum을 사용할 수 있다.
