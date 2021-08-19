5c91244f854d5d8b3f831d1d53205fcd3ece8e39
JaeGeun.Song <jae99c@gmail.com>
2021-08-19 15:14:28 +0900

    Predefined Value Parameters with Enumeration

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+from enum import Enum
 from fastapi import FastAPI
+"""
+Enum 열거형을 이용한 사전 정의 값
 
+"""
+
+class ModelName(str, Enum): # string enum
+    alexnet ="alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+# 값은 3개 중 1개가 들어가야함 안그럼 오류
 app =FastAPI()
 
-@app.get("/users/me") # 이건 고정경로
-async def read_user_me():
-    return {"user_id" : "the current user"}
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet: #모델이름이 알렉스넷
+        return {"model_name": model_name, "message" : "Deep Learning FTW!"}
 
-@app.get("/users/{user_id}") # 이건 매개변수 경로
-async def read_user(user_id: str):
-    return {"user_id" : user_id}
+    if model_name.value == "lenet": #모델이름 값이 lenet
+        return {"model_name": model_name, "message": "LeCNN all the images"}
 
-"""
-    고정 경로와 매개변수 경로가 같이 있으면 고정 경로를 먼저 선언하고 후에 매개변수 경로를 선언해야한다.
-    /users/{user_id} 이전에 /users/me를 선언해야한다.
-    
-    그렇지 않으면 /users/{user_id} 는 매개변수 user_id의 값을 me라고 생각하여 /users/me도 연결한다.
-"""
+    return {"model_name ": model_name, "message":"Have some residuals"} # resnet
