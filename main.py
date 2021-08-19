f35be6919fc79299b26ae24125305dcb05db1c76
JaeGeun.Song <jae99c@gmail.com>
2021-08-19 16:50:05 +0900

    essential query parameter level 1

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 """
-여러 경로/쿼리 매개변수
-multiple path and query parameter
+필수 쿼리 매개변수
+essential query parameter level 1
+경로가 아닌 매개변수에 대한 기본값을 선언할 때
+특정값을 추가하지 않고 선택적으로 만들기 위해서 기본값을 None으로 설정하면 되었지만
 
-경로 매개변수와 쿼리 매개변수를 동시에 선언할 수 있고 fastAPI는 어느것이 무엇인지 알고 있다.
-그리고 특정 순서로 선언할 필요없다.
-
-매개변수들은 이름으로 감지된다.
+쿼리 매개변수를 필수로 만들려면 기본값을 선언할 수 없다.
 """
-from typing import Optional
 from fastapi import FastAPI
 
-app =FastAPI()
+app = FastAPI()
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(user_id: int, item_id:str, q: Optional[str] = None, short: bool = False):
-    item = {"item_id":item_id, "owner_id":user_id}
-    if q:
-        item.update({"q":q})
-    if not short:
-        item.update({"description":"This is an amazing item that has a long description"})
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}  # 쿼리 매개변수의 기본값을 선언하지 않으면 필수가 된다.
     return item
+#needy는 필수 매개변수 이므로 반드시 URL에 설정해주어야한다.
