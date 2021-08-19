e882eca6d82acbfa54af2b6b1dd1f72bc3d59d76
JaeGeun.Song <jae99c@gmail.com>
2021-08-19 16:16:23 +0900

    type cast of query parameter

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/items/{item_id}")
-async def read_item(item_id : str, q : Optional[str] = None): # 매개변수 q는 선택적이며 기본값으로 None값이 된다.
-    # 자료형 쓰는 곳에 Optional[str] 형식으로 작성한다. - 기본값은 None
-    if q: # q가 존재하면 매개변수 q도 같이 response한다.
-        return {"item_id":item_id, "q":q}
-    return {"item_id":item_id}
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False): # bool형으로 선언할 수도 있다.
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short: # short 값이 있는가 없는가를 bool로 판단한다.
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
