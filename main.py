b2251b4a29297053e7807db20c3be60187b6d584
JaeGeun.Song <jae99c@gmail.com>
2021-08-20 10:32:38 +0900

    Request body + path parameters

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
-
-# 첫째로 pydantic의 BaseModel을 import 해야 한다.
+"""
+Request body + path parameters
+requestbody와 path parameter를 동시에 선언할 수 있다.
+fastapi는 경로 매개변수와 일치하는 함수 매개변수를 경로에서 가져와야한다.
+그리고 Pydantic모델로 선언된 함수 매개변수를 request body에서 가져와야한다는 것을 인지한다.
+"""
 from typing import Optional
-
 from fastapi import FastAPI
-from pydantic import BaseModel  # import pydantic's BaseModel
+from pydantic import BaseModel
 
+class Item(BaseModel):
+    name : str
+    description : Optional[str] = None
+    price :float
+    tax : Optional[float] = None
 
-class Item(BaseModel):  # Create your data model : BaseModel을 상속 받는 데이터 모델 클래스 선언
-    # 모든 속성 타입을 위해 파이썬 정규문법을 사용한다.
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
+app =FastAPI()
 
-
-
-
-app = FastAPI()
-
-@app.post("/items/") # request body에 담아서 보내기
-async def create_item(item: Item): # 매개변수로 선언
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax # 함수 내에서 모델 객체의 모든 속성에 직접 액세스 할 수 있다.
-        item_dict.update({"price_with_tax":price_with_tax})
-    return item_dict
-
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item:Item):
+    return {"item_id":item_id, **item.dict()}
